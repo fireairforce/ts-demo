bb593aeb8d0f73145446ffcd58ea4fece884b9b5
fireairforce <1344492820@qq.com>
2019-09-21 14:59:54 +0800

    feat: add commander

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,21 @@
 
 // console.log(`hhh`);
 
+import colors from "colors";
+import commander from "commander";
+
+const command = commander
+  .version("0.1.0")
+  .option("-c --city [name]", "Add city name")
+  .parse(process.argv);
+
+if (process.argv.slice(3).length === 0) {
+  command.outputHelp(colors.red);
+  process.exit();
+}
+
+// if (!command.city) {
+//   command.outputHelp();
+// }
+// console.log(command.city);
+// yarn ts-node src/index.ts -h
